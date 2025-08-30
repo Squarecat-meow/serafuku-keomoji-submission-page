d159a734be35c3bb368c62ceceb1990952811358
Squarecat-meow <yozumina@serafuku.moe>
2025-08-30 23:27:37 +0900

    💄 style(mainpage): 파비콘, 메인 페이지 작성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,56 @@
-import Image from "next/image";
+'use client';
+
+import Image from 'next/image';
+import SerafukuIcon from '@/public/images/serafuku.png';
+import MisskeyIcon from '@/public/icons/Misskey.svg';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { misskeyAuthPhaseOne } from '@/actions/auth/misskeyAuthPhaseOne';
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const router = useRouter();
+  const handleLoginClick = async () => {
+    try {
+      const { url } = await misskeyAuthPhaseOne();
+      router.push(url);
+    } catch (err) {
+      if (err instanceof Error) console.error(err.message);
+    }
+  };
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+  return (
+    <main className="hero min-h-screen">
+      <section className="hero-content text-center">
+        <article className="space-y-8 break-keep">
+          <h1 className="text-6xl font-bold">
+            세라복.모에 커스텀 에모지 신청 페이지
+          </h1>
+          <p>
+            미스키
+            <MisskeyIcon className="w-5 h-5 inline-flex fill-green-600 mx-1" />
+            인스턴스{' '}
+            <Link
+              href={'https://serafuku.moe'}
+              className="text-sky-500 underline"
+            >
+              세라복.모에
+            </Link>
+            의 커스텀 이모지를 신청할 수 있는 페이지입니다. <br />
+            세라복.모에 계정으로만 로그인할 수 있으며, <br />
+            신청 후에는 학생회가 검토한 후, 승인 결과를 DM으로 알려드립니다.
+          </p>
+          <button className="btn btn-xl bg-sky-500" onClick={handleLoginClick}>
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src={SerafukuIcon}
+              alt="세라복.모에 아이콘"
+              width={24}
+              height={24}
+              className="mr-1"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+            세라복.모에로 로그인
+          </button>
+        </article>
+      </section>
+    </main>
   );
 }
