f6793378057d4b76c23ba7e7dadccf0d84ecdfa5
Squarecat-meow <yozumina@serafuku.moe>
2025-08-31 09:49:19 +0900

    💄 style(Main): 내부 컨텐츠 배경 하얗게 설정

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,4 +1,4 @@
-import NavBar from '@/components/primitives/Navbar';
+import NavBar from "@/components/primitives/Navbar";
 
 export default function Layout({
   children,
@@ -7,7 +7,9 @@ export default function Layout({
     <section className="w-screen p-4">
       <section className="w-full md:w-3/4 m-auto">
         <NavBar />
-        {children}
+        <article className="my-4 p-4 bg-base-100 text-base-content rounded-2xl shadow">
+          {children}
+        </article>
       </section>
     </section>
   );
