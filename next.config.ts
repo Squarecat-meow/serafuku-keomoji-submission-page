f3e9d93c455023d2d010315722ceff93a9eea0d7
Squarecat-meow <yozumina@serafuku.moe>
2025-09-11 11:07:57 +0900

    feat(proxy): 이미지 프록시 작성

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,23 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  rewrites: async () => [
+    {
+      source: "/api/media/:path*",
+      destination:
+        "https://yunomina-blog.s3.us-east-005.backblazeb2.com/:path*",
+    },
+    {
+      source: "/api/emojis/:path*",
+      destination:
+        "https://yunomina-blog.s3.us-east-005.backblazeb2.com/emojis/:path*",
+    },
+  ],
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ['@svgr/webpack'],
+      use: ["@svgr/webpack"],
     });
 
     return config;
