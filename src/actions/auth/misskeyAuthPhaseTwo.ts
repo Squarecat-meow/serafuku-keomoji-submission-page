597858caa18d69c57e3ed498d3298e810056e72f
Squarecat-meow <yozumina@serafuku.moe>
2025-09-12 23:42:05 +0900

    feat(misskeyAuth): 미스키로 로그인 후 유저정보 DB에 넣기

--- src/actions/auth/misskeyAuthPhaseTwo.ts
+++ src/actions/auth/misskeyAuthPhaseTwo.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { signJwt } from "@/lib/jwt";
 import { prisma } from "@/lib/prismaClient";
 import { IUser } from "@/types/auth/authType";
 import ky from "ky";
@@ -32,8 +33,33 @@ export async function misskeyAuthPhaseTwo(token: string) {
         },
       )
       .json();
+    const accessToken = await signJwt({ username: accessInfo.user.username });
+    cookieStore.set("misskeyAccessToken", accessInfo.accessToken, {
+      maxAge: 1000 * 60 * 15,
+    });
+    cookieStore.set("accessToken", accessToken, {
+      maxAge: 1000 * 60 * 15,
+    });
 
-    cookieStore.set("accessToken", accessInfo.accessToken);
+    await prisma.user.upsert({
+      where: {
+        username: accessInfo.user.username,
+      },
+      update: {
+        name: accessInfo.user.name,
+        username: accessInfo.user.username,
+        avatarUrl: accessInfo.user.avatarUrl,
+        bannerUrl: accessInfo.user.bannerUrl,
+        roles: accessInfo.user.roles.map((el) => el.id),
+      },
+      create: {
+        name: accessInfo.user.name,
+        username: accessInfo.user.username,
+        avatarUrl: accessInfo.user.avatarUrl,
+        bannerUrl: accessInfo.user.bannerUrl,
+        roles: accessInfo.user.roles.map((el) => el.id),
+      },
+    });
 
     return accessInfo.user;
   } catch (err) {
