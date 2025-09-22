8b242f90aaa4d54247f74214d86cfe4bd9918a7f
Squarecat-meow <yozumina@serafuku.moe>
2025-09-22 12:02:43 +0900

    style(SubmissionCard): 카드 위에 호버해도 모달 띄워지게 수정

--- src/app/main/_components/SubmissionCard.tsx
+++ src/app/main/_components/SubmissionCard.tsx
@@ -11,42 +11,52 @@ export default function SubmissionCard({
   setData: (id: Submission) => void;
   setIsVisible: (state: boolean) => void;
 }) {
+  const variant = {
+    rest: {
+      scale: 1,
+      rotateZ: 0,
+    },
+    hover: {
+      scale: 1.1,
+      rotateZ: 3,
+    },
+  };
   return (
-    <article className="w-full p-4 flex flex-col gap-4 bg-base-200 rounded-2xl">
-      <div
-        className="w-1/3 aspect-square m-auto"
-        onClick={() => {
-          setData(data);
-          setIsVisible(true);
-        }}
-      >
-        <motion.img
-          whileHover={{ scale: 1.1, rotateZ: 3 }}
-          src={data.url}
-          alt={`:${data.name}: 커모지`}
-          className="w-full aspect-square rounded-2xl object-contain cursor-pointer"
-        />
-      </div>
+    <motion.article
+      className="w-full p-4 flex flex-col gap-4 bg-base-200 rounded-2xl cursor-pointer"
+      initial="rest"
+      whileHover="hover"
+      onClick={() => {
+        setData(data);
+        setIsVisible(true);
+      }}
+    >
+      <motion.img
+        variants={variant}
+        src={data.url}
+        alt={`:${data.name}: 커모지`}
+        className="w-1/3 m-auto aspect-square rounded-2xl object-contain"
+      />
       <div className="space-y-2">
-        <div>
-          <div className="flex gap-4 items-center">
-            <h1 className="text-2xl font-bold">:{data.name}:</h1>
-            <Chip status={data.status} />
-          </div>
-          <h2 className="text-lg text-gray-500">{data.category}</h2>
+        <div className="flex gap-4 items-center">
+          <h1 className="text-2xl font-bold">:{data.name}:</h1>
+          <Chip status={data.status} />
         </div>
-        <div className="space-x-2">
-          {data.aliases.map((el, i) => (
-            <span
-              key={i}
-              className="px-3 py-1 text-sm text-gray-600 dark:text-gray-300 border border-gray-300 dark:border-gray-600 rounded-2xl"
-            >
-              {el}
-            </span>
-          ))}
+        <div className="flex items-center gap-4">
+          <h2 className="text-lg text-gray-500">{data.category}</h2>
+          <div className="space-x-2">
+            {data.aliases.map((el, i) => (
+              <span
+                key={i}
+                className="px-3 py-1 text-sm text-gray-600 dark:text-gray-300 border border-gray-300 dark:border-gray-600 rounded-2xl"
+              >
+                {el}
+              </span>
+            ))}
+          </div>
         </div>
         <p>신청한 학생: {data.submissionerUsername}</p>
       </div>
-    </article>
+    </motion.article>
   );
 }
