0ca4beeb45998e75f656a624cae2ce7c3eca5395
Squarecat-meow <yozumina@serafuku.moe>
2025-09-23 10:21:38 +0900

    WIP: 커모지 승인시에 미스키에도 승인되게

--- src/app/api/admin/stat/route.ts
+++ src/app/api/admin/stat/route.ts
@@ -1,6 +1,7 @@
-import { Submission } from "@/generated/prisma";
+import { Submission } from "@prisma/client/index.js";
 import { prisma } from "@/lib/prismaClient";
 import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
 export async function GET(req: NextRequest) {
   try {
@@ -32,10 +33,33 @@ export async function GET(req: NextRequest) {
 
 export async function PATCH(req: NextRequest) {
   try {
+    const cookieStore = await cookies();
+    const hasMisskeyToken = cookieStore.has("misskeyAccessToken");
+
+    if (!hasMisskeyToken)
+      return NextResponse.json(
+        { message: "미스키 토큰이 없습니다!" },
+        { status: 401 },
+      );
+
+    const misskeyToken = cookieStore.get("misskeyAccessToken")!.value;
+
     const data = (await req.json()) as {
       id: number;
       status: Submission["status"];
     };
+
+    if (data.status === "ACCEPTED") {
+      const { url: imageUrl } = await prisma.submission.findUniqueOrThrow({
+        where: {
+          id: data.id,
+        },
+        select: {
+          url: true,
+        },
+      });
+    }
+
     const res = await prisma.submission.update({
       where: {
         id: data.id,
